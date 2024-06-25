31e4c15fc5c05b29cecf25890b1de46c8cea7294
madhavkrishangarg <108024625+madhavkrishangarg@users.noreply.github.com>
2024-06-26 00:28:30 +0530

    updated env variables

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
     host: process.env.RAILWAY_MYSQL_HOST,
+    port: process.env.RAILWAY_MYSQL_PORT,
     user: process.env.RAILWAY_MYSQL_USER,
     password: process.env.RAILWAY_MYSQL_PASSWORD,
     database: process.env.RAILWAY_MYSQL_DATABASE,
