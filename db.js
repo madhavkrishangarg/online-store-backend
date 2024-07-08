569b89b929790084c6266aad87a0586dcbfc1215
madhavkrishangarg <108024625+madhavkrishangarg@users.noreply.github.com>
2024-07-08 14:49:10 +0530

    updates connection pool

--- db.js
+++ db.js
@@ -3,21 +3,25 @@ require('dotenv').config();
 
 let pool;
 
+async function createPool() {
+    return mysql.createPool({
+        host: process.env.RAILWAY_MYSQL_HOST,
+        port: process.env.RAILWAY_MYSQL_PORT || 3306,
+        user: process.env.RAILWAY_MYSQL_USER,
+        password: process.env.RAILWAY_MYSQL_PASSWORD,
+        database: process.env.RAILWAY_MYSQL_DATABASE,
+        connectionLimit: 10,
+        queueLimit: 0,
+        waitForConnections: true,
+        enableKeepAlive: true,
+        keepAliveInitialDelay: 0,
+    });
+}
+
 async function handleDisconnect() {
     try {
-        pool = mysql.createPool({
-            host: process.env.RAILWAY_MYSQL_HOST,
-            port: process.env.RAILWAY_MYSQL_PORT || 3306,
-            user: process.env.RAILWAY_MYSQL_USER,
-            password: process.env.RAILWAY_MYSQL_PASSWORD,
-            database: process.env.RAILWAY_MYSQL_DATABASE,
-            connectionLimit: 10,
-        });
-
-        // Test the connection
-        const connection = await pool.getConnection();
-        console.log('Successfully connected to the database.');
-        connection.release();
+        pool = await createPool();
+        console.log('Successfully connected to the database. :', pool);
     } catch (err) {
         console.error('Failed to connect to the database:', err);
         setTimeout(handleDisconnect, 2000);
@@ -32,7 +36,7 @@ module.exports = {
             throw new Error('Database connection not established');
         }
         try {
-            const [results] = await pool.query(sql, params);
+            const [results] = await pool.execute(sql, params);
             return results;
         } catch (error) {
             console.error('Database query error:', error);
